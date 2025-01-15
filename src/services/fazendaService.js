2b1e79a34df1d92f4da747f26f4d552a53305e20
Pedro Bassani <pedro.bassani@expressosaomiguel.com.br>
2025-01-15 10:33:10 -0300

    corrigido código da fazenda

--- src/services/fazendaService.js
+++ src/services/fazendaService.js
@@ -2,66 +2,46 @@ const Fazenda = require("../models/fazendaModel.js");
 
 module.exports = {
   async criar({ responsavel_id, nome }) {
-    try {
-      const fazenda = await Fazenda.create({ responsavel_id, nome });
-      return fazenda;
-    } catch (error) {
-      throw new Error("Erro ao criar a fazenda: " + error.message);
-    }
+    const fazenda = await Fazenda.create({ responsavel_id, nome });
+    return fazenda;
   },
 
-  async atualizar({ id, responsavel_id, nome }) {
-    try {
-      const fazenda = await Fazenda.update(
-        { responsavel_id, nome },
-        {
-          where: { id },
-          returning: true,
-          plain: true,
-        }
-      );
-
-      if (fazenda[0] === 0) {
-        throw new Error("Fazenda não encontrada ou não foi atualizada");
-      }
-
-      return fazenda[1];
-    } catch (error) {
-      throw new Error("Erro ao atualizar a fazenda: " + error.message);
+  async atualizar(fazenda) {
+    const antiga = await this.pegar(fazenda.id);
+    if (!antiga) {
+      throw new Error("Fazenda não encontrada");
     }
+    await Fazenda.update(
+      { responsavel_id: fazenda.responsavel_id, nome: fazenda.nome },
+      {
+        where: { id: fazenda.id },
+        returning: true,
+        plain: true,
+      }
+    );
+
+    return fazenda;
   },
 
   async pegar(id) {
-    try {
-      const fazenda = await Fazenda.findByPk(id);
-      if (!fazenda) {
-        throw new Error("Fazenda não encontrada");
-      }
-      return fazenda;
-    } catch (error) {
-      throw new Error("Erro ao buscar a fazenda: " + error.message);
+    const fazenda = await Fazenda.findByPk(id);
+    if (!fazenda) {
+      throw new Error("Fazenda não encontrada");
     }
+    return fazenda;
   },
 
   async deletar(id) {
-    try {
-      const fazenda = await Fazenda.destroy({
-        where: { id },
-      });
-      if (!fazenda) {
-        throw new Error("Fazenda não encontrada ou não foi deletada");
-      }
-    } catch (error) {
-      throw new Error("Erro ao deletar a fazenda: " + error.message);
+    const fazenda = await Fazenda.destroy({
+      where: { id },
+    });
+    if (!fazenda) {
+      throw new Error("Fazenda não encontrada ou não foi deletada");
     }
   },
 
   async listar() {
-    try {
-      const fazendas = await Fazenda.findAll();
-      return fazendas;
-    } catch (error) {
-      throw new Error("Erro ao listar as fazendas: " + error.message);
-    }
+    const fazendas = await Fazenda.findAll();
+    return fazendas;
   },
 };
